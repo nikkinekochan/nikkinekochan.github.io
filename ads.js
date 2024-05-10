bbed6817c238dc3528e95b57b600ef86b7b02b6d
nikkinekochan <89461050+nikkinekochan@users.noreply.github.com>
2024-05-10 13:39:05 +0200

    Update ads.js

--- ads.js
+++ ads.js
@@ -50,7 +50,7 @@ function initializeIMA() {
   });
 
   var adsRequest = new google.ima.AdsRequest();
-  adsRequest.adTagUrl = 'https://7cd77.v.fwmrm.net/ad/g/1?nw=511351&prof=511351:ftv_ebu_web&csid=ebuweb&caid=ebu_videos&sfid=17040999&afid=339800939&resp=vast4&vdur=60&flag=+exvt+sltp+slcb&metr=1023';
+  adsRequest.adTagUrl = 'https://pubads.g.doubleclick.net/gampad/ads?iu=/21643048729/TESTVIDEO_SIWEI&description_url=[placeholder]&tfcd=0&npa=0&sz=624x352%7C640x480&gdfp_req=1&unviewed_position_start=1&output=xml_vast4&env=vp&impl=s&correlator=';
 
   // Specify the linear and nonlinear slot sizes. This helps the SDK to
   // select the correct creative if multiple are returned.
