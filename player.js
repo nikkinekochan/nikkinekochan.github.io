f45719501609088441e150bc28960e01be75de30
nikkinekochan <89461050+nikkinekochan@users.noreply.github.com>
2024-08-01 11:45:47 +0200

    Update player.js

--- player.js
+++ player.js
@@ -17,8 +17,8 @@ tv.freewheel.DemoPlayer = function() {
 	var theNetworkId = 511351;
 	var theServerURL = "https://7cd77.v.fwmrm.net/ad/g/1";
 	var theProfileId = "511351:ftv_brut_sdk_web"
-	var theVideoAssetId = "www.brut.fr/test";
-	var theSiteSectionId  = "brut_testsite";
+	var theVideoAssetId = "www.brut.fr/nature";
+	var theSiteSectionId  = "www.brut.fr";
 	var theVideoDuration = 500;
 
 	// Step #2: Initialize AdManager
